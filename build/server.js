9c8da7c1005c751c697ae2761598fcba2dcd999e
Tintu Jose <tintjose@gmail.com>
2018-10-25 13:48:29 -0700

    Initial commit

---
+++ build/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var axios = require('axios');
+
+var app = express();
+var port = process.env.PORT || 5001;
+app.listen(port, function() {
+  console.log('We are live on ' + port);
+});
+
+var API_URL = 'https://www.metaweather.com/api/location/';
+// router and controller for cities list
+app.get('/cities', function(req, res) {
+  axios.get(API_URL + 'search/?query=' + req.query.input).then(
+    function(data) {
+      res.status(200).send(data.data);
+    },
+    function(error) {
+      res.status(500).json(error);
+    }
+  );
+});
+
+// router and controller for weather
+app.get('/weather', function(req, res) {
+  axios.get('' + API_URL + req.query.input).then(
+    function(data) {
+      res.status(200).send(data.data);
+    },
+    function(error) {
+      res.status(500).json(error);
+    }
+  );
+});
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+//import path from 'path';
+const path=require('path');
+
+const app = express();
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log('port no ' +port);
+});
+
+// get reference to the client build directory
+
+// pass the static files (react app) to the express app.
+app.use(express.static(path.join(__dirname, '../../client/build')));
+
+const API_URL = 'https://www.metaweather.com/api/location/';
+// router and controller for cities list
+app.get('/cities', (req, res) => {
+  console.log('req.query.input ', req.query.input);
+  axios.get(`${API_URL}search/?query=${req.query.input}`).then(
+    data => {
+      console.log('data ', data.data);
+      res.status(200).send(data.data);
+    },
+    error => {
+      res.status(500).json(error);
+    }
+  );
+});
+
+// router and controller for weather
+app.get('/weather', (req, res) => {
+  console.log('req.query.input ', req.query.input);
+  axios.get(`${API_URL}${req.query.input}`).then(
+    data => {
+      console.log('data ', data.data);
+      res.status(200).send(data.data);
+    },
+    error => {
+      res.status(500).json(error);
+    }
+  );
+});
